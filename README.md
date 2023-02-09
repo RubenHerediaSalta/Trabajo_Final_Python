b62fafbe6916857d267584ff8e72d96d838f01e8
unknown <ruben.a.her@gmail.com>
2023-02-08 21:05:28 -0300

    added README

---
+++ README.md
@@ -0,0 +1,9 @@
+Hola, acá presento el trabajo final del curso de Python, estéticamente no es de mis mejores trabajos pero las funcionalidades están completas.
+
+Se trabajó sobre un e-Commerce dedicado a la venta de cuentas de videojuegos y servicio de coaching.
+
+Se bloquearon las vistas de inicio de sesión y de registro para usuarios que ya tienen iniciada la sesión, que fue algo que no vimos en clases y para acceder al CRUD de cuentas, coaches y servicios se creó una vista tipo panel de administración, que solo es accesible si el usuario conectado tiene permisos de administrador.
+
+Espero que sea de su agrado.
+
+¡Muchas Gracias!.
--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=100, null=True)
-    last_name = models.CharField(max_length=100, null=True)
-    phone = models.CharField(max_length=30, null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_images', null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=True, verbose_name='Nombre')
+    last_name = models.CharField(max_length=100, null=True, verbose_name='Apellido')
+    phone = models.CharField(max_length=30, null=True, blank=True, verbose_name='Telefono')
+    profile_picture = models.ImageField(upload_to='profile_images', null=True, blank=True, verbose_name='Imagen de perfil')
